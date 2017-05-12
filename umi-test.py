2c9bbfb009daa89e817227e8d4b1d75ec22168a9
Nick de Wolf <nickdewolf@msn.com>
2017-05-12 13:08:26 +0200

    Removed Scaling, fixed Camera, fixed joint locations, fixed allowed angles, set display to degrees instead of radians, set sizes to meters.

--- umi-test.py
+++ umi-test.py
@@ -3,9 +3,6 @@
 # By Andrew Lee
 # Math 198 Fall 2009
 
-#Choose a location to move to by pressing keys 0-9
-#Press enter (return) to execute
-
 from __future__ import division, print_function
 from visual import *
 from visual.graph import *
@@ -33,10 +30,7 @@ lower_length = 0.2535
 lower_height = 0.080
 
 # Dimensions wrist
-wrist_height = 0.189
-
-# UMI SCALER
-umi_scale = 4.0
+wrist_height = 0.09
 
 #**********************************************
 # Functions that are called on various events
@@ -50,35 +44,25 @@ def rotateShoulderNeg(evt): # called on "Rotate right" button event
 def leave(evt): # called on "Exit under program control" button event
     exit()
 
-def setred(evt): # called by "Make red" menu item
-    pass
-
-def setcyan(evt): # called by "Make cyan" menu item
-    pass
-
-def cuberate(value):
-    pass
-
-def setRiserHeight(evt): # called on slider events
+def setRiserHeight(evt): # called on slider events (output in mm)
     value = s0.GetValue() / 1000.0
-    s0_label = wx.StaticText(p, pos=(1.0*L,0.15*L), label='Set Riser Height: '+ str(value))
-    riser.pos = ((wpedestal*umi_scale)/2.0, value, 0)
+    s0_label.SetLabel('Set Riser Height: %d mm' % (value * 1000 / 2.0))
+    riser.pos = (wpedestal/2.0, value, 0)
 
-def setShoulderAngle(evt): # called on slider events
+def setShoulderAngle(evt): # called on slider events (output in degrees)
     value = s1.GetValue() / 1000.0
-    s1_label = wx.StaticText(p, pos=(1.0*L,0.35*L), label='Set Shoulder rotation: '+ str(value))
+    s1_label.SetLabel('Set Shoulder rotation: %.2f degrees' % degrees(value))
     shoulder_joint.axis = (cos(value),0,sin(value))
 
-def setElbowAngle(evt): # called on slider events
+def setElbowAngle(evt): # called on slider events (output in degrees)
     value = s2.GetValue() / 1000.0
-    s2_label = wx.StaticText(p, pos=(1.0*L,0.60*L), label='Set Elbow rotation: '+ str(value))
+    s2_label.SetLabel('Set Elbow rotation: %.2f degrees' % degrees(value))
     elbow_joint.axis = (cos(value),0,sin(value))
 
-def setWristAngle(evt): # called on slider events]
+def setWristAngle(evt): # called on slider events] (output in degrees)
     value = s3.GetValue() / 1000.0
-    s3_label = wx.StaticText(p, pos=(1.0*L,0.85*L), label='Set Wrist rotation: '+ str(value))
+    s3_label.SetLabel('Set Wrist rotation: %.2f degrees' % degrees(value))
     wrist_joint.axis = (cos(value),0,sin(value))
-
 
 L = 600
 # Create a window. Note that w.win is the wxPython "Frame" (the window).
@@ -93,7 +77,7 @@ w = window(width=2*(L+window.dwidth), height=L+window.dheight+window.menuheight,
 
 # Place a 3D display widget in the left half of the window.
 d = 20
-disp = display(window=w, x=d, y=d, width=L-2*d, height=L-2*d, forward=-vector(0,1,2))
+disp = display(window=w, x=d, y=d, width=L-2*d, height=L-2*d, forward=-vector(0,0.25,0.5), center=vector(0,0.5,0))
 
 # Place buttons, radio buttons, a scrolling text object, and a slider
 # in the right half of the window. Positions and sizes are given in
@@ -106,24 +90,24 @@ wx.StaticText(p, pos=(d,4), size=(L-2*d,d), label='3D representation.',
 toggle_grip = wx.Button(p, label='Toggle Grip', pos=(1.5*L+10,15))
 toggle_grip.Bind(wx.EVT_BUTTON, rotateShoulderNeg)
 
-max_height = 0.5*(hpedestal-pedestal_offset)*umi_scale*1000.0
-min_height = 0
+max_height = 0.5*(hpedestal-pedestal_offset)*1000.0
+min_height = -0.5*(pedestal_offset+upper_height+lower_height+wrist_height)*1000.0
 
 s0 = wx.Slider(p, pos=(1.0*L,0.2*L), size=(0.9*L,20), minValue=min_height, maxValue=max_height)
 s0.Bind(wx.EVT_SCROLL, setRiserHeight)
-s0_label = wx.StaticText(p, pos=(1.0*L,0.15*L), label='Set Riser height: '+str(0.9*0.5*hpedestal*umi_scale))
+s0_label = wx.StaticText(p, pos=(1.0*L,0.15*L), label='Set Riser height: %d mm' % (0.9 * max_height / 2.0))
 
-s1 = wx.Slider(p, pos=(1.0*L,0.4*L), size=(0.9*L,20), minValue=0.5*-pi*1000.0, maxValue=0.5*pi*1000.0)
+s1 = wx.Slider(p, pos=(1.0*L,0.4*L), size=(0.9*L,20), minValue=radians(-90)*1000.0, maxValue=radians(90)*1000.0)
 s1.Bind(wx.EVT_SCROLL, setShoulderAngle)
-s1_label = wx.StaticText(p, pos=(1.0*L,0.35*L), label='Set Shoulder rotation: 0')
+s1_label = wx.StaticText(p, pos=(1.0*L,0.35*L), label='Set Shoulder rotation: 0 degrees')
 
-s2 = wx.Slider(p, pos=(1.0*L,0.65*L), size=(0.9*L,20), minValue=0.5*-pi*1000.0, maxValue=0.5*pi*1000.0)
+s2 = wx.Slider(p, pos=(1.0*L,0.65*L), size=(0.9*L,20), minValue=radians(-180)*1000.0, maxValue=radians(110)*1000.0)
 s2.Bind(wx.EVT_SCROLL, setElbowAngle)
-s2_label = wx.StaticText(p, pos=(1.0*L,0.60*L), label='Set Elbow rotation: 0')
+s2_label = wx.StaticText(p, pos=(1.0*L,0.60*L), label='Set Elbow rotation: 0 degrees')
 
 s3 = wx.Slider(p, pos=(1.0*L,0.9*L), size=(0.9*L,20), minValue=0.5*-pi*1000.0, maxValue=0.5*pi*1000.0, style=wx.SL_HORIZONTAL)
 s3.Bind(wx.EVT_SCROLL, setWristAngle)
-s3_label = wx.StaticText(p, pos=(1.0*L,0.85*L), label='Set Wrist rotation: 0')
+s3_label = wx.StaticText(p, pos=(1.0*L,0.85*L), label='Set Wrist rotation: 0 degrees')
 
 # Create a menu of options (Rotate right, Rotate right, Make red, Make cyan).
 # Currently, menus do not work on the Macintosh.
@@ -140,92 +124,74 @@ m.Append(menu, 'Options')
 # ROBOT JOINTS
 frameworld = frame()
 frame0 = frame(frame=frameworld)
-frame0.pos = (-(wpedestal*umi_scale)/2.0, 0.5*hpedestal*umi_scale,0)
+frame0.pos = (-wpedestal/2.0, 0.5*hpedestal,0)
 
 # The shoulder joint location is now on world position (x,z) = (0,0)
 riser = frame(frame=frame0)
-riser.pos = ((wpedestal*umi_scale)/2.0,0.9*frame0.pos.y, 0)
+riser.pos = (wpedestal/2.0,0.9*frame0.pos.y, 0)
 
 shoulder_joint = frame(frame=riser)
-shoulder_joint.pos = (0,-(pedestal_offset*umi_scale), 0)
+shoulder_joint.pos = (0,-pedestal_offset, 0)
 #shoulder_joint.rotate(axis = (0, 1, 0), angle = pi/4)
 
 elbow_joint = frame(frame=shoulder_joint)
-elbow_joint.pos = ((upper_length*umi_scale),-(upper_height*umi_scale), 0)
+elbow_joint.pos = (upper_length,-upper_height, 0)
 #elbow_joint.rotate(axis = (0, 1, 0), angle = pi/4)
 
 wrist_joint = frame(frame=elbow_joint)
-wrist_joint.pos = ((lower_length*umi_scale),-(lower_height*umi_scale), 0)
+wrist_joint.pos = (lower_length,-lower_height, 0)
 #wrist_joint.rotate(axis = (0, 1, 0), angle = pi/4)
 #************************************************
 # ROBOT ARM
-pedestal = box(frame = frame0, 
+pedestal = box(frame = frame0,
                pos = (0,0,0),
-               height = hpedestal*umi_scale,
-               length = wpedestal*umi_scale,
-               width = wpedestal*umi_scale,
+               height = hpedestal,
+               length = wpedestal,
+               width = wpedestal,
                color = (0.4, 0.4, 0.4))
-pointer_riser = box(frame = riser,
-               pos = (0,0,0),
-               height = 1,
-               length = 0.1,
-               width = 0.1,
-               color = color.blue)
 riser_part = cylinder(frame = riser,
-               pos = (0, -pedestal_offset*umi_scale/2, 0),
-               axis = (0, pedestal_offset*umi_scale, 0),
-               radius = wpedestal*umi_scale/2,
+               pos = (0, -pedestal_offset/2.0, 0),
+               axis = (0, pedestal_offset, 0),
+               radius = wpedestal/2.0,
                color = color.red)
-                
-pointer_shoulder = box(frame = shoulder_joint,
-               pos = (0,0,0),
-               height = 1,
-               length = 0.1,
-               width = 0.1,
-               color = color.blue)
+
 upper_arm = box(frame = shoulder_joint,
-               pos = ((upper_length*umi_scale)/2.0,0,0),
-               height = (upper_height*umi_scale),
-               length = (upper_length*umi_scale)*1.2,
-               width = 0.08*umi_scale,
+               pos = (upper_length/2.0,0,0),
+               height = upper_height,
+               length = upper_length*1.2,
+               width = 0.08,
                color = color.green)
-pointer_elbow = box(frame = elbow_joint,
-               pos = (0,0,0),
-               height = 1,
-               length = 0.1,
-               width = 0.1,
-               color = color.blue)
 lower_arm = box(frame = elbow_joint,
-               pos = ((lower_length*umi_scale)/2.0,0,0),
-               height = (lower_height*umi_scale),
-               length = (lower_length*umi_scale)*1.2,
-               width = 0.08*umi_scale,
+               pos = (lower_length/2.0,0,0),
+               height = lower_height,
+               length = lower_length*1.2,
+               width = 0.08,
                color = color.green)
 pointer_wrist = box(frame = wrist_joint,
-               pos = (0,-(hpedestal*umi_scale)/2.0,0),
-               height = hpedestal*umi_scale,
-               length = 0.1,
-               width = 0.1,
+               pos = (0,-0.3/2.0,0),
+               height = 0.1,
+               length = 0.05,
+               width = 0.05,
                color = color.blue)
 wrist = box(frame = wrist_joint,
                pos = (0,0,0),
-               height = wrist_height*umi_scale,
-               length = 0.08*umi_scale,
-               width = 0.08*umi_scale,
+               height = wrist_height,
+               length = 0.08,
+               width = 0.08,
                color = color.green)
 
 floor = box(frame=frameworld,
                pos = (0,0,0),
                height = 0.001,
-               length = wpedestal*umi_scale + 0.6*umi_scale,
-               width = 0.6*umi_scale*2,
+               length = wpedestal + 0.6,
+               width = 0.6*2,
                color = (0.5, 0.5, 0.5))
-floor.pos = (floor.length/2 - (wpedestal*umi_scale), 0, 0)
+floor.pos = (floor.length/2 - wpedestal, 0, 0)
 #**************************************************************************
 # CHESSBOARD
 
 # Dimensions of the board
-chessboard_size = umi_scale / 4.0
+chessboard_size = 0.25
 field_size = (chessboard_size / 8.0)
 chessboard_dist = chessboard_size/2 - field_size
 
@@ -234,7 +200,7 @@ wallthck = field_size / 15.0
 wallhght = field_size / 15.0
 
 # Position of the center of the board
-mplhght = 0.2
+mplhght = 0.02
 mplcent = chessboard_size
 
 # Colors of the board
@@ -271,7 +237,7 @@ for field in range(8):
                length = mchessboard.length,
                width = wallthck,
                pos = (chessboard_dist, 0.5*wallhght, beam_offset+(0.5*wallthck)-mchessboard.width/2),
-               color = beam_color)                      
+               color = beam_color)
     )
 width_beams.append(box(frame = framemp,
                height = wallhght,
@@ -292,7 +258,7 @@ for x in range(8):
     for y in range(8):
         if (x + y) % 2 == 0:
             fields.append( box(frame = framemp,
-                   height = 0.01,
+                   height = 0.001,
                    length = field_size,
                    width = field_size,
                    pos = (field_size*x - field_size/2.0, 0, (chessboard_dist - field_size*y) + field_size/2),
@@ -309,8 +275,15 @@ s3.SetValue(0) # update the slider
 # CREATE CONTROLS
 while(True):
     rate(100)
-
+    disp.center=frame0.frame_to_world(
+        riser.frame_to_world(
+            shoulder_joint.frame_to_world(
+                elbow_joint.frame_to_world(
+                    wrist_joint.pos - vector(0,wrist.height/2.0, 0)
+                )
+            )
+        )
+    )
 #End Program
 
 0
-
