a6a258c25bc2e484285dc6603fd80ccff297c057
Nick de Wolf <nickdewolf@msn.com>
2017-05-19 16:41:03 +0200

    Fixed the toCartesian function.

--- umi_parameters.py
+++ umi_parameters.py
@@ -1,31 +1,31 @@
 from __future__ import division, print_function
 
 class UMI_parameters:
-	def __init__(self):
-		# Specifications of UMI
-		# Zed
-		self.hpedestal = 1.082
-		self.pedestal_offset = 0.0675
-		self.wpedestal = 0.1 # Undefined in real robot
+    def __init__(self):
+        # Specifications of UMI
+        # Zed
+        self.hpedestal = 1.082
+        self.pedestal_offset = 0.0675
+        self.wpedestal = 0.1 # Undefined in real robot
 
-		# Dimensions upper arm
-		self.upper_length = 0.2535
-		self.upper_height = 0.095
+        # Dimensions upper arm
+        self.upper_length = 0.2535
+        self.upper_height = 0.095
 
-		# Dimensions lower arm
-		self.lower_length = 0.2535
-		self.lower_height = 0.08
+        # Dimensions lower arm
+        self.lower_length = 0.2535
+        self.lower_height = 0.08
 
-		# Dimensions wrist
-		self.wrist_height = 0.09
+        # Dimensions wrist
+        self.wrist_height = 0.09
 
-		# Height of the arm from the very top of the riser, to the tip of the gripper.
-		self.total_arm_height = self.pedestal_offset + self.upper_height \
-								+ self.lower_height + self.wrist_height
-	
-	def correct_height(self, y):
-		'''
-			Function that corrects the y value of the umi-rtx, because the real arm runs from
-			from -self.hpedestal/2 to self.hpedestal/2, while y runs from 0 to self.hpedestal.
-		'''
-		return y - 0.5*self.hpedestal
+        # Height of the arm from the very top of the riser, to the tip of the gripper.
+        self.total_arm_height = self.pedestal_offset + self.upper_height \
+                                + self.lower_height + self.wrist_height
+    
+    def correct_height(self, y):
+        '''
+            Function that corrects the y value of the umi-rtx, because the real arm runs from
+            from -self.hpedestal/2 to self.hpedestal/2, while y runs from 0 to self.hpedestal.
+        '''
+        return y - 0.5*self.hpedestal
--- umi_student_functions.py
+++ umi_student_functions.py
@@ -1,6 +1,7 @@
 from __future__ import division, print_function
 from umi_parameters import UMI_parameters
 import math
+import numpy as np
 from visual import *
 # Specifications of UMI
 # Enter the correct details in the corresponding file (umi_parameters.py).
@@ -31,3 +32,59 @@ def apply_inverse_kinematics(x, y, z):
     shoulder_angle = degrees(math.atan2(z,x) - math.atan2(arm_2*s2,arm_1 + arm_2*c2))
     wrist_angle = - elbow_angle - shoulder_angle
     return riser_position, shoulder_angle, elbow_angle, wrist_angle, left_handed
+
+def board_position_to_cartesian(chessboard, position, real_world):
+    ''' Convert a position between [a1-h8] to its cartesian coordinates in frameworld coordinates.
+    
+        You are not allowed to use the functions such as: frame_to_world.
+        You have to show actual calculations using positions/vectors and angles.
+        
+        :param obj chessboard: The instantiation of the chessboard that you wish to use.
+        :param str position: A position in the range [a1-h8]
+        
+        :return: tuple Return a position in the format (x,y,z)
+    '''
+    # Get the local coordinates for the tiles on the board in the 0-7 range.
+    row = ord(position[0]) - ord('a')
+    column = int(position[1]) - 1
+    
+    # Retrieve the translation and the rotation of the board
+    board_position = chessboard.get_position()
+    board_angle = chessboard.get_angle_radians()
+
+    # Initialize the first tile of the board, closes to the robot (h8)
+    cartesian_row = chessboard.field_size * (7.0 - row) + chessboard.field_size/2.0
+    cartesian_col = chessboard.field_size * (7.0 - column) + chessboard.field_size/2.0
+    
+    # Compute the length of the vector
+    length_vector = math.sqrt(cartesian_row * cartesian_row + cartesian_col * cartesian_col);
+    # Compute the angle between the rotation center and the vector.
+    position_angle = atan2(cartesian_row,cartesian_col) 
+    # Add the rotation of the board to this angle
+    total_angle = -board_angle+position_angle
+    
+    # compute the new row/column values
+    rotated_cartesian_row = length_vector * sin(total_angle)
+    rotated_cartesian_col = length_vector * cos(total_angle)    
+    
+    # Output the results.
+    result = (rotated_cartesian_row+board_position[0], board_position[1], rotated_cartesian_col+board_position[2])
+    return result
+    
+    #TODO: Remove markers.
+    box(frame = real_world,
+               pos = board_position,
+               height = 0.3,
+               length = 0.005,
+               width = 0.005,
+               color = color.red)
+    
+    box(frame = real_world,
+               pos = result,
+               height = 0.3,
+               length = 0.005,
+               width = 0.005,
+               color = color.blue)
+    
+    
+    
